43a6546b7daa569ec092ca679dbc2be7a8485435
Najib2989 <mahmudnajib26@gmail.com>
2025-10-05 19:13:06 +0600

    Add files via upload

---
+++ Sunny_Day.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; i++) {
+       int X;
+       scanf("%d",&X);
+       if(X>24){
+           printf("YES\n");
+       }else{
+           printf("NO\n");
+       }
+    }
+
+    return 0;
+}
