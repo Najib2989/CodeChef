62382c4240572e45b696f564074ed260947a5891
Najib2989 <mahmudnajib26@gmail.com>
2025-10-11 02:23:34 +0600

    Add files via upload

---
+++ Candy_Division.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   int T;
+   cin >> T;
+   for(int i=0; i<T; i++){
+    int N;
+    cin >> N;
+    if(N%3==0){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+    }
+return 0;
+}
