1db8152c3716495c27e0ccdd1233ae131970d677
Najib2989 <mahmudnajib26@gmail.com>
2025-10-12 00:53:11 +0600

    Add files via upload

---
+++ Right_There.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int T;
+    cin >> T;
+    for(int i=0; i<T; i++){
+        int N,X;
+        cin >> N >> X;
+        if(N<=X){
+            cout << "YES" << endl;
+        }else {
+
+            cout << "NO" << endl;
+        }
+    }
+return 0;
+}
