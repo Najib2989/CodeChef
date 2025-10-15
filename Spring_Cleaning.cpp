65aaee18fed8c73f2bdcc685e7f728b9fb8f0217
Najib2989 <mahmudnajib26@gmail.com>
2025-10-15 20:51:25 +0600

    Add files via upload

---
+++ Spring_Cleaning.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int X,Y;
+    cin >> X >> Y;
+    int total_time = (X*30) + (Y*60);
+    cout << total_time << endl;
+
+    return 0;
+}
