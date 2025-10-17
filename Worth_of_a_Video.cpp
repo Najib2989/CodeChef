89429bbe3d2e2783cada6e9a34710e12b15626da
Najib2989 <mahmudnajib26@gmail.com>
2025-10-18 01:46:15 +0600

    Add files via upload

---
+++ Worth_of_a_Video.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+   int T;
+   cin >> T;
+   for(int i=0; i<T; i++)
+   {
+       int S,Y;
+       cin >> S;
+       Y = S * 24;
+       int total_words = Y * 1000;
+       cout << total_words << endl;
+
+   }
+
+ return 0;
+}
